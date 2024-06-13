aa2d59ac03d1a4f450b6526bef41ac753ddcdaba
cristianhdz25 <cristian.hernandez@ucr.ac.cr>
2024-06-12 21:04:33 -0600

    ACtual

--- index.php
+++ index.php
@@ -1,42 +1,9 @@
 <?php
 
-header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET,POST,PUT, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 $endpoint = $_GET['endpoint'] ?? '';
-
-if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    if ($endpoint === 'images' && isset($_GET['targetDir'])) {
-        require_once 'controllers/ImageController.php';
-        $imageController = new ImageController();
-        $targetDir = $_GET['targetDir'];
-        $imagePath = $imageController->getImageByFilePath($targetDir);
-
-        echo json_encode(['imageUrl' => $imagePath]);
-
-        if ($imagePath !== null && file_exists($imagePath)) {
-            // Detectar el tipo de imagen y establecer la cabecera correcta
-            $imageInfo = getimagesize($imagePath);
-            if ($imageInfo === false) {
-                header("HTTP/1.1 500 Internal Server Error");
-                echo json_encode(['error' => 'Error al obtener información de la imagen']);
-                exit();
-            }
-
-            header("Content-Type: " . $imageInfo['mime']);
-            header("Content-Length: " . filesize($imagePath));
-            header("HTTP/1.1 200 OK");
-
-            // Leer y enviar el contenido del archivo de imagen
-            readfile($imagePath);
-            exit();
-        } else {
-            header("HTTP/1.1 404 Not Found");
-            echo json_encode(['error' => 'Image not found']);
-            exit();
-        }
-    }
-}
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     header("HTTP/1.1 200 OK");
@@ -52,7 +19,7 @@ if ($_POST['METHOD'] === 'POST') {
         $response = $imageController->upload($image);
     } else {
         $response['status'] = 'error';
-        $response['message'] = 'Sorry, there was an error uploading your file.';
+        $response['message'] = 'Lo sentimos, hubo un error al subir su imagen.';
 
     }
     echo json_encode($response);
